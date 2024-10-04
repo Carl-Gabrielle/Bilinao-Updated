8aaba6a04d4691fccd51219966cb6523ed2d29e3
Carl Gabrielle <orfinadacarlgabrielle@gmail.com>
2024-10-04 17:54:40 +0800

    Checkout Retrieval

--- resources/js/Pages/Customer/Checkout.jsx
+++ resources/js/Pages/Customer/Checkout.jsx
@@ -11,11 +11,7 @@ import axios from "axios";
 import Label from "@/Components/Label";
 import ReadOnly from "@/Components/ReadOnly";
 
-<<<<<<< HEAD
 export default function Checkout({ auth, carts, product }) {
-=======
-export default function Checkout({ auth, product }) {
->>>>>>> 2669f969f83ca7e392cc618288e4a46529d59097
     const { user } = auth;
     const [cartItems, setCartItems] = useState(carts || []);
     const [productItems, setProductItems] = useState(product ? [product] : []);
@@ -103,19 +99,6 @@ export default function Checkout({ auth, product }) {
         });
     };
 
-<<<<<<< HEAD
-=======
-    const [cartItems, setCartItems] = useState([product]); // Store the single product instead of multiple carts
-
-    // Calculate total price based on product price and quantity
-    const calculateTotal = () => {
-        return cartItems.reduce(
-            (acc, item) => acc + item.price * item.quantity,
-            0
-        );
-    };
-
->>>>>>> 2669f969f83ca7e392cc618288e4a46529d59097
     return (
         <CustomerLayout user={auth.user}>
             <Head title="Checkout" />
@@ -300,7 +283,6 @@ export default function Checkout({ auth, product }) {
                                 </div>
                                 <div className="bg-slate-50 p-6 space-y-4 h-72 overflow-y-auto scroll-bar">
                                     <h1 className="font-medium">Your Order</h1>
-<<<<<<< HEAD
 
                                     <div>
                                         {itemsToRender.map((item) => (
@@ -357,39 +339,6 @@ export default function Checkout({ auth, product }) {
                                             </div>
                                         ))}
                                     </div>
-=======
-                                    {cartItems.map((cart) => (
-                                        <div key={cart.id} className="flex">
-                                            <div className="relative border">
-                                                <img
-                                                    src={`/storage/${cart.images[0].image_path}`}
-                                                    alt={cart.name}
-                                                    className="sm:size-16 size-10 object-cover rounded"
-                                                />
-                                                <div className="absolute -top-3 flex text-slate-100 items-center justify-center -right-3 size-5 bg-slate-700 rounded-full">
-                                                    <span className="text-xs">
-                                                        {cart.quantity}
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <div className="flex-1 ml-4 text-xs text-slate-800">
-                                                <h3 className="font-semibold">
-                                                    {cart.name}
-                                                </h3>
-                                                <p className="flex items-center">
-                                                    <FaPesoSign className="inline-block mr-1" />
-                                                    {Number(
-                                                        cart.price
-                                                    ).toLocaleString("en-US", {
-                                                        minimumFractionDigits: 2,
-                                                        maximumFractionDigits: 2,
-                                                    })}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    ))}
-
->>>>>>> 2669f969f83ca7e392cc618288e4a46529d59097
                                     <div className="flex justify-between text-sm border-t pt-4">
                                         <span className="font-semibold">
                                             Subtotal
