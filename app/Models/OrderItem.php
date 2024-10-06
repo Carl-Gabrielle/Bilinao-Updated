dc3a6b47fd25d2cc4fa616caca97749a6731681b
Elmer <lejero08@gmail.com>
2024-10-06 11:07:12 +0800

    Model: Relationship of orderitem and order

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -21,4 +21,9 @@ class OrderItem extends Model
         'delivered_date'
     ];
     use HasFactory;
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
 }
